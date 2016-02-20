f816857313f282406a597c5b9c702eaffeb01c04
Tam Thanh Tam <xitrum4692@gmail.com>
2016-02-20 02:24:56 +0000

    pjs support - was s3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tam Thanh Tran' });
+  res.render('index.ejs', { title: 'Tam Thanh Tran' });
 });
 
 module.exports = router;
